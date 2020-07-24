fd34a929d2b0e599efb77c13e3dea5f63215c93a
Chaminda Fernando <chaminda.fernando@smartengine.solutions>
2020-07-24 15:26:10 +0200

    Initial commit

---
+++ src/com/mcdf/test/module/Products.java
@@ -0,0 +1,14 @@
+package com.mcdf.test.module;
+
+public class Products {
+
+    private String name;
+
+    public Products(String name){
+        this.name =name;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
